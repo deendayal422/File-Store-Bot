dd94a5c7ee014bdb9873b663a69d02c50ac40c03
deendayal422 <149863092+deendayal422@users.noreply.github.com>
2023-12-08 20:42:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,7 +74,7 @@ async def start(bot: Client, cmd: Message):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Updates Channel", url="https://t.me/Deendayal_dhakad")
+                        InlineKeyboardButton("Share And Support Us, url= "https://t.me/Public_File_storage_Bot")
                     ]
                 ]
             )
